467db014abea0177645592f2fff8a959c85e29d9
AlbFR <albertoferrada20@gmail.com>
2023-05-30 00:54:03 -0400

    Added Boundary Class in utilities.h

--- utilities.h
+++ utilities.h
@@ -9,20 +9,49 @@ class Point {
 	public:
 		int x;
 		int y;
+
+		Point() {
+			throw "Cannot create a Point Object without x and y arguments";
+		}
+
 		Point(int x, int y) {
 			this->x = x;
 			this->y = y;
 		}
+
 		void print() {
 			std::cout << "(" << this->x << ", " << this->y << ")";
 		}
+
 		void setX(int x) {
 			this->x = x;
 		}
+
 		void setY(int y) {
 			this->y = y;
 		}
 };
 
+class Boundary {
+	public:
+		Point upperleft;
+		Point lowerright;
+
+		Boundary(Point ul, Point lr) {
+			if (ul.x > lr.x || ul.y > lr.y)
+				throw "Cannot create Boundary, upperLeft Point must be lower than lowerRight Point";
+			
+			this->upperleft = ul;
+			this->lowerright = lr;
+		}
+
+		void print() {
+			std::cout << "Upper Left bound: ";
+			upperleft.print();
+			std::cout << std::endl << "Lower Right bound: ";
+			lowerright.print();
+			std::cout << std::endl;
+		}
+};
 
 #endif
