c7a1bc923a1fbefcb824dd5cfcd4d14eab21aa8a
AlbFR <albertoferrada20@gmail.com>
2023-06-23 20:48:55 -0400

    PointList has now its own file

---
+++ src/utilities/PointList.h
@@ -0,0 +1,24 @@
+#ifndef PointList_h
+#define PointList_h
+
+#include <vector>
+#include "Point.h"
+
+class PointList {
+	public:
+	 	double x, y;
+		std::vector<Point*> container;
+		// PointList();
+		PointList(Point *p);
+		int population();
+		bool sameCoordsAs(Point p);
+		void append(Point *p);
+		void append(PointList *pl);
+		unsigned size() const;
+		void print() const;
+
+	private:
+		unsigned population_;
+};
+
+#endif
